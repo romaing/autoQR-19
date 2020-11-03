b22a41ee311527c19f5c0c1048ed2b1c96e2d5d4
Hugo <hderre@dixit-productions.com>
2020-11-03 11:44:57 +0100

    reverse postal_code & city fields

--- index.php
+++ index.php
@@ -68,13 +68,13 @@
                         'type' => 'text',
                         'label' => 'Code Postal',
                         'size' => 'col-6',
-                        'value' =>  $_COOKIE['city'] ?? ''
+                        'value' =>  $_COOKIE['postal_code'] ?? ''
                     ],
                     'city' => [
                         'type' => 'text',
                         'label' => 'Ville',
                         'size' => 'col-6',
-                        'value' =>  $_COOKIE['postal_code'] ?? ''
+                        'value' =>  $_COOKIE['city'] ?? ''
                     ],
                 ];
 
@@ -115,19 +115,6 @@
         <p><br><em>Ce site à recourt à l'utilisation de cookies utiles à ce que vous n'ayez pas à retaper sans cesse vos informations !</em></p>
     </div>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <script>
-        // $('#submit').prop("disabled", true);
-
-        // $('.type-sortie-input').change(function() {
-        //     $('div.checkbox-group.required :checkbox:checked').length > 0 ? $('#submit').prop("disabled", false) : $('#submit').prop("disabled", true);
-        // });
-
-        // $('#submit').click(function() {
-        //     if($('#submit').disabled) {
-        //         console.log('disabled')
-        //     }
-        // });
-    </script>
 </body>
 
 </html>
